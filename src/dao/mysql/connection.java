b993ca52e5f2b57675b736a45482b65b79bf0709
David Romero <d.romero@sapalomera.cat>
2025-04-07 15:37:30 +0200

    connection file added

--- src/dao/mysql/connection.java
+++ src/dao/mysql/connection.java
@@ -1,9 +1,29 @@
 package dao.mysql;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class connection {
-    public static void main(String[] args) {
-        String url = "http://localhost:3306/escalada";
+    private static Connection openConnection(String url){
+        Connection connection = null;
+
         String user = "root";
         String password = "";
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException error){
+            System.out.println(error.getMessage());
+        }
+
+        return connection;
+    }
+
+    private static void closeConnection(Connection connection){
+        try {
+            if (connection != null) connection.close();
+        } catch (SQLException error){
+            System.out.println(error.getMessage());
+        }
     }
 }
--- src/dao/mysql/connection_example.java
+++ src/dao/mysql/connection_example.java
@@ -1,4 +1,31 @@
 package dao.mysql;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class connection_example {
+    //Open connection
+    private static Connection openConnection(String url){
+        Connection connection = null;
+
+        String user = ""; //your db username
+        String password = ""; //your db password
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);  //get connection by the url
+        } catch (SQLException error){
+            System.out.println(error.getMessage()); //catch error if connection fails
+        }
+
+        return connection; //return connection in case of success
+    }
+
+    //Close connection
+    private static void closeConnection(Connection connection){
+        try {
+            if (connection != null) connection.close();
+        } catch (SQLException error){
+            System.out.println(error.getMessage());
+        }
+    }
 }
