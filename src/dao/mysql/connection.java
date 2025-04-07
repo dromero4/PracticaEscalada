4c486c09048f240f28bee4c236bf72a36e16cd5b
David Romero <147045613+dromero4@users.noreply.github.com>
2025-04-07 15:30:14 +0200

    Delete src/dao/mysql/connection.java

--- src/dao/mysql/connection.java
+++
@@ -1,9 +0,0 @@
-package dao.mysql;
-
-public class connection {
-    public static void main(String[] args) {
-        String url = "http://localhost:3306/escalada";
-        String user = "root";
-        String password = "";
-    }
-}
